8d32d8ea2c7ac75c1a7902c454a48a081b283c2c
BlancheMiret <blanchemiret@gmail.com>
2025-08-26 17:52:52 +0200

    Make huggingFace request dynamic

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,3 @@
-# Définir les endpoints
-
-# Requête du front seront : "donne moi un mot, avec ces mots"
-# Et dans le futur : en évitant ce mot, ces mots...
-
-# GET ?
-# - word 1 
-# - word 2
-# - word 3
-
-# localhost:5000/endpoint_name
-
 from flask import request, jsonify
 from config import app
 from huggingface_hub import InferenceClient
@@ -21,20 +9,24 @@ client = InferenceClient(api_key=HUGGINGFACE_API_KEY)
 
 @app.route("/get_code_name", methods=["GET"])
 def get_code_name():
-    word_1 = "queen"
-    word_2 = "salt"
+    word_1 = request.json.get("word1")
+    word_2 = request.json.get("word2")
+    word_3 = request.json.get("word3")
+
+    if not word_1:
+        return jsonify({"message":"You mest at least complete the word_1 field."}), 400
 
     completion = client.chat.completions.create(
         model = MODEL,
         messages = [
             {
                 "role": "user",
-                "content": f"Generate a single word that is related to the following words: {word_1}, {word_2}. Give me only the word."
+                "content": f"Generate a single word that is related to the following words: {word_1}, {word_2}, {word_3}. Give me only the word."
             }
         ],
     )
     code_name = completion.choices[0].message['content']
-    return jsonify({"codeName":code_name})
+    return jsonify({"codeName":code_name}), 200
 
 if __name__ == "__main__":
     app.run(debug=True)
